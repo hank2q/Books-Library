d4cc5f22d1d984288662bb63e5db2e8d10961cc6
Hank <hassanien.kaissi@gmail.com>
2021-05-14 22:45:09 +0400

    changed firebase export

--- src/contexts/BooksContext.js
+++ src/contexts/BooksContext.js
@@ -1,10 +1,10 @@
 import { createContext, useState, useEffect } from "react";
-import fireDB from "../firebaseDB";
+import fireBase from "../firebaseDB";
 
 export const BooksContext = createContext();
 
 export function BooksProvider({ children }) {
-    const db = fireDB.collection("books");
+    const db = fireBase.firestore().collection("books");
     const [books, setBooks] = useState([]);
     const [items, setItems] = useState(db.orderBy("title", "asc"));
 
@@ -44,20 +44,6 @@ export function BooksProvider({ children }) {
         </BooksContext.Provider>
     );
 }
-// items.onSnapshot((bs) => {
-//     bs.forEach((b) => {
-//         fetchedBooks.push({ ...b.data(), id: b.id });
-//     });
-//     setBooks(fetchedBooks);
-// });
-
-// items.get().then((fireBooks) => {
-//     let fetchedBooks = [];
-//     fireBooks.forEach((fireBook) => {
-//         fetchedBooks.push({ ...fireBook.data(), id: fireBook.id });
-//     });
-//     setBooks(fetchedBooks);
-// });
 
 // const fireBooks = await items.get();
 // fireBooks.forEach((fireBook) => {
--- src/firebaseDB.js
+++ src/firebaseDB.js
@@ -1,4 +1,5 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
 const firebaseConfig = {
     apiKey: "AIzaSyB4BrYz08qiq2OFr5ef1bJRJvbFkiqCCUI",
     authDomain: "my-books-310519.firebaseapp.com",
@@ -8,5 +9,5 @@ const firebaseConfig = {
     appId: "1:688475310544:web:233e7a668d4b4c131991d5",
 };
 // Initialize Firebase
-const fireDB = firebase.initializeApp(firebaseConfig);
-export default fireDB.firestore();
+const fireBase = firebase.initializeApp(firebaseConfig);
+export default fireBase;
